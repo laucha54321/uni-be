d762350e6654a7ebd7de286161f14cc5b4303fd0
Laureano Oliva <laureanoliva@gmail.com>
2023-11-03 01:37:34 -0300

    drizzle studio

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  driver: "mysql2",
+  dbCredentials: {
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST!,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE!,
+    port: 3306,
+  },
+} satisfies Config;
