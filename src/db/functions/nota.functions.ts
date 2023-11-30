bdbd450b7289df29eba4e39ea458ca001433461a
Laureano Oliva <laureanoliva@gmail.com>
2023-11-30 15:44:32 -0300

    notas query

--- src/db/functions/nota.functions.ts
+++ src/db/functions/nota.functions.ts
@@ -1,11 +1,19 @@
 import { db } from "../client";
-import { notas, insertNotasSchemaNoID, insertNotasSchema } from "../schema/notas.schema";
+import {
+  notas,
+  insertNotasSchemaNoID,
+  insertNotasSchema,
+  insertNotasSchemaNoIDs,
+} from "../schema/notas.schema";
 import crypto from "crypto";
-import { Static } from '@sinclair/typebox'
+import { Static } from "@sinclair/typebox";
 import { eq } from "drizzle-orm";
+import { personas } from "../schema/persona.schema";
+import { cursos } from "../schema/curso.schema";
 
-type insertNotaNoIDType = Static<typeof insertNotasSchemaNoID>
-type insertNota = Static<typeof insertNotasSchema>
+type insertNotaNoIDType = Static<typeof insertNotasSchemaNoID>;
+type insertNota = Static<typeof insertNotasSchema>;
+type insertNotasNoIDs = Static<typeof insertNotasSchemaNoIDs>;
 
 export const notaInsert = async (aux: insertNotaNoIDType) => {
   aux = {
@@ -20,7 +28,41 @@ export const notaInsert = async (aux: insertNotaNoIDType) => {
   return result;
 };
 
-export const notaSelect = async (id:string) => {
-  const result = await db.select().from(notas).where(eq(notas.id, id))
+export const notaSelect = async (id: string) => {
+  // const result = await db.query.notas.findMany({
+  //   with: {
+  //     personas: true,
+  //   },
+  // });
+  const result = await db
+    .select({
+      id: notas.id,
+      calificacion: notas.calificacion,
+      descripcion: notas.descripcion,
+      curso: cursos.nombre,
+      nombre: personas.nombres,
+      dni: personas.dni,
+      email: personas.email,
+    })
+    .from(notas)
+    .leftJoin(personas, eq(personas.id, notas.id_persona))
+    .leftJoin(cursos, eq(cursos.id, notas.id_curso))
+    .where(eq(notas.id, id));
+  // const result = await db.select().from(notas).leftjoin();
   return result;
-}
+};
+
+export const notaUpdate = async (aux: insertNotasNoIDs) => {
+  const id = aux.id;
+
+  delete aux.id_curso;
+  delete aux.id_persona;
+
+  const result = await db.update(notas).set(aux).where(eq(notas.id, id));
+  return result;
+};
+
+export const notaDelte = async (id: string) => {
+  const result = await db.delete(notas).where(eq(notas.id, id));
+  return result;
+};
--- src/routes/api/nota.routes.ts
+++ src/routes/api/nota.routes.ts
@@ -1,54 +1,76 @@
 import * as fastify from "fastify";
-import { insertNotasSchema,selectNotaSchema, id } from "../../db/schema/notas.schema";
-import { notaInsert, notaSelect } from "../../db/functions/nota.functions";
+import {
+  insertNotasSchema,
+  selectNotaSchema,
+  id,
+  insertNotasSchemaNoID,
+  insertNotasSchemaNoIDs,
+} from "../../db/schema/notas.schema";
+import {
+  notaInsert,
+  notaSelect,
+  notaDelte,
+  notaUpdate,
+} from "../../db/functions/nota.functions";
 import { Value } from "@sinclair/typebox/value";
-import { handler, uriSchema, uriType} from "../errorHandler";
+import { handler, uriType } from "../errorHandler";
 
 async function router(app: fastify.FastifyInstance) {
-  app.get("/:id",{
-    schema:{
-      response:{
-        200:selectNotaSchema,
-      }
-    }
-  }, async (request, reply) => {
-    
-    if(Value.Check(id,request.params)){
-      const result = await notaSelect(request.params.id)
-      if(result.length>0){
-        return result[0]
-      }else{
-        reply.code(404).send({});
-      }
-    }else{
-      reply.code(400).send({});
+  app.get("/:id", async (request, reply) => {
+    const uri = { uri: "4a108bee-920a-488a-a21e-1fa1695cf11d" };
+    if (Value.Check(id, request.params)) {
+      const result = await notaSelect(request.params.id);
+      return result;
+      // if (result.length > 0) {
+      //   return result[0];
+      // } else {
+      //   reply.code(404).send(uri);
+      // }
+    } else {
+      reply.code(400).send(uri);
     }
   });
   app.post("/", async (request, reply) => {
-    const uri = {uri:""}
-    if (Value.Check(insertNotasSchema, request.body)) {
+    const uri = { uri: "c6f582dd-c403-43c6-9b29-fb48c8f72a2a" };
+    if (Value.Check(insertNotasSchemaNoID, request.body)) {
       const result = notaInsert(request.body);
       return result;
     } else {
-      reply.code(400).send({});
+      reply.code(400).send(uri);
     }
   });
-  app.patch("/", async () => {
-    const uri = {uri:""}
-    return { hello: "nota Router" };
+  app.patch("/", async (request, reply) => {
+    const uri = { uri: "33801332-a281-4d3a-88a2-0a5979cb96fc" };
+    if (Value.Check(insertNotasSchemaNoIDs, request.body)) {
+      const result = notaUpdate(request.body);
+      return result;
+    } else {
+      reply.code(400).send(uri);
+    }
   });
-  app.delete("/", async () => {
-    const uri = {uri:""}
-    return { hello: "nota Router" };
+  app.delete("/:id", async (request, reply) => {
+    const uri = { uri: "b0c33bf7-fce2-4839-85f5-83a0a5464580" };
+
+    if (Value.Check(id, request.params)) {
+      const result = await notaDelte(request.params.id);
+      if (result[0]["affectedRows"] > 0) {
+        reply.send(result[0]);
+      } else {
+        reply.code(404).send(uri);
+      }
+    } else {
+      reply.code(400).send(uri);
+    }
   });
-  app.addHook("preSerialization", (request,reply,payload,done) =>{
-    if(reply.statusCode>=300){
-      const aux = payload as uriType
-      const err = null
-      const response = handler(reply.statusCode,"Nota", aux.uri);
-      done(err, response)
-    }else{
-      done()
+
+  app.addHook("preSerialization", (request, reply, payload, done) => {
+    if (reply.statusCode >= 300) {
+      const aux = payload as uriType;
+      const err = null;
+      const response = handler(reply.statusCode, "Nota", aux.uri);
+      done(err, response);
+    } else {
+      done();
     }
   });
 }
